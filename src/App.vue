a0260e4ea407728c83a99071ac2c32d0b45ccc8b
Christopher <ChrFimbel@gmail.com>
2022-12-23 11:06:14 -0700

    Clean app.

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,9 @@
 <script setup lang="ts">
-import { reactive } from 'vue';
-
-import NewTaskInput from '@/components/NewTaskInput.vue';
-import Task from '@/components/Task.vue';
-import TaskInterface from '@/types/Task';
-import useTask from '@/composables/useTask';
-
-const tasks: TaskInterface[] = reactive([]);
-
-function handleSubmit(name: string) {
-  tasks.push(useTask(name));
-}
-
-function handleUpdateComplete(newValue: boolean, index: number) {
-  const targetTask = tasks[index];
-  targetTask.complete = newValue;
-}
 </script>
 
 <template>
   <div class="container">
-    <Task
-      v-for="(task, index) in tasks"
-      :key="index"
-      :task="task"
-      @update:complete="handleUpdateComplete($event, index)"
-    />
-    <NewTaskInput
-      id="new-task"
-      @submit="handleSubmit"
-    />
+    <div />
   </div>
 </template>
 
--- src/types/Task.ts
+++
@@ -1,4 +0,0 @@
-export default interface Task {
-  complete: boolean;
-  name: string;
-}
