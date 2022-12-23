10467a4897c5a8568e6fd9683ea753b5711d7af0
Christopher <ChrFimbel@gmail.com>
2022-12-23 15:55:29 -0700

    Revert "WIP"
    This reverts commit 20f8827a74cf588e75ec39076941a7bad0b38717.

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,17 @@
 <script setup lang="ts">
-import {
-  reactive,
-  ref,
-} from 'vue';
-
-import Task from '@/components/Task.vue';
-import TaskInterface from '@/types/Task';
-
-const tasks = reactive<TaskInterface[]>([]);
-const nextTaskId = ref(1);
-
-function getNextTaskId() {
-  const id = nextTaskId.value;
-
-  nextTaskId.value += 1;
-
-  return id;
-}
-
-function addTask() {
-  const newTask = {
-    complete: false,
-    id: getNextTaskId(),
-    name: 'A new task',
-  };
-
-  tasks.push(newTask);
-}
 </script>
 
 <template>
-  <main
-    class="main"
-  >
-    <Task
-      v-for="(task, index) in tasks"
-      :key="index"
-      :task="task"
-    />
-    <button
-      type="button"
-      @click="addTask"
-    >
-      Add Task
-    </button>
-  </main>
+  <div class="container">
+    <div />
+  </div>
 </template>
 
 <style lang="scss">
 @import './sass/base/reset';
 @import './sass/base/typography';
 
-.main {
+.container {
   max-width: 100rem;
   background-color: #eee;
   margin: 10rem auto;
--- src/types/Task.ts
+++
@@ -1,5 +0,0 @@
-export default interface Task {
-  complete: boolean;
-  id: number;
-  name: string;
-}
