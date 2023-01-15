04dd3fed0733ed6e3add3c660aee8d234922f42a
Christopher <ChrFimbel@gmail.com>
2023-01-15 14:59:08 -0700

    Add comment.

--- src/composables/useTaskStore.ts
+++ src/composables/useTaskStore.ts
@@ -112,6 +112,7 @@ export default defineStore('tasks', () => {
       return;
     }
 
+    // Update indeterminate in parent task
     updateTaskIndeterminate(parent.id, getTaskIndeterminateBasedOnSubtasks(parent.id));
   }
 
